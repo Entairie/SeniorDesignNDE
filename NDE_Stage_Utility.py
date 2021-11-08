d745317af256e8543d4504e572c529eec91e8685
Entairie <rionkrampe@gmail.com>
2021-11-07 20:38:53 -0600

    Clean up and organize code
    Moved variables around.
    Made reusable variables global in Utility.
    Moved user input variables out of Utility.
    Reworked scan to include indents in y-axis for scans.
    Made pause in Y movement for "scan" for future coder to place Scan functiuon

--- NDE_Stage_Utility.py
+++ NDE_Stage_Utility.py
@@ -1,6 +1,8 @@
 import pigpio
 import time
 
+pi = pigpio.pi()
+
 # Enable pigpio on command line prior to running code using ~~> sudo pigpiod
 # Disable after code using ~~> sudo killall pigpiod
 
@@ -8,14 +10,20 @@ import time
 # 24 - x Step           22 - y Step         6 - z Step          Blue      
 # 10 - x Limit          9 - y limit         11 - z limit        Grey
 
+yzdelay = 0.00025
+xdelay = 0.0025
+# x Direction
+#       0 ~~> Clockwise ~~> towards motor
+#       1 ~~> Counter   ~~> away
+# yz Direction
+#       1 ~~> Clockwise ~~> towards motor
+#       0 ~~> Counter   ~~> away
 
-pi = pigpio.pi()
-
-delay = 0.00025
-steps = 1600
-Dir = 0
-        # 0 ~~> Clockwise ~~> towards motor
-        # 1 ~~> Counter   ~~> away
+# Precision and Threaded leads
+#       12.5 um / step        1 step = 0.0000125 m
+#       3.175 um / step       1 step = 0.000003175 m
+PreStepConv = 0.0000125
+TheStepConv = 0.000003175
 
 def StartUpZero():
     pi.set_mode(10, pigpio.INPUT)           # Set pin 10 as input
@@ -27,43 +35,39 @@ def StartUpZero():
     
     pi.write(23, 0)     # x Dir low ~~> towards motor
     pi.write(27, 1)     # y Dir high ~~> towards motor
-    pi.write(5, 0)      # z Dir low ~~> away motor
+    pi.write(5, 0)      # z Dir low ~~> away motor/ up
     
     # Zero X
     input_state_x = True      # Switch not pushed
     while input_state_x == True:  # While switch not pushed
         pi.write(24, 1)         # Set local pi's GPIO BCM 10 high
-        time.sleep(0.0025)
+        time.sleep(xdelay)
         pi.write(24, 0)         # Set local pi's GPIO BCM 10 low
-        time.sleep(0.0025)
+        time.sleep(xdelay)
         input_state_x = pi.read(10)   # reads 1/True/not pushed or 0/False/pushed
     
     # Zero Y
     input_state_y = True
     while input_state_y == True:
         pi.write(22, 1)
-        time.sleep(delay)
+        time.sleep(yzdelay)
         pi.write(22, 0)
-        time.sleep(delay)
+        time.sleep(yzdelay)
         input_state_y = pi.read(9)
     
-    # Zero z
+    # Zero Z
     input_state_z = True
     while input_state_z == True:
         pi.write(6, 1)
-        time.sleep(delay)
+        time.sleep(yzdelay)
         pi.write(6, 0)
-        time.sleep(delay)
+        time.sleep(yzdelay)
         input_state_z = pi.read(11)
 
 def MoveToHome(x, y, z):
     # Moves sensor from zero to home to start scan
     # x, y, z are distances in m that Sensor will move to home point
 
-    # 12.5 um / step        1 step = 0.0000125 m
-    # 3.175 um / step       1 step = 0.000003175 m
-    PreStepConv = 0.0000125
-    TheStepConv = 0.000003175
     xStep = round(x/PreStepConv)
     yStep = round(y/TheStepConv)
     zStep = round(z/TheStepConv)
@@ -72,51 +76,53 @@ def MoveToHome(x, y, z):
     pi.write(27, 0)     # y Dir ~~> away motor
     pi.write(5, 1)      # z Dir ~~> towards motor
 
-
     # Move to x home
     for x in range(xStep):
         pi.write(24, 1)         # Set local pi's GPIO BCM 10 high
-        time.sleep(0.0025)
+        time.sleep(xdelay)
         pi.write(24, 0)         # Set local pi's GPIO BCM 10 low
-        time.sleep(0.0025)
+        time.sleep(xdelay)
     
     # Move to y home
     for y in range(yStep):
         pi.write(22, 1)
-        time.sleep(delay)
+        time.sleep(yzdelay)
         pi.write(22, 0)
-        time.sleep(delay)
+        time.sleep(yzdelay)
     
     # Move to z home
     for z in range(zStep):
         pi.write(6, 1)
-        time.sleep(delay)
+        time.sleep(yzdelay)
         pi.write(6, 0)
-        time.sleep(delay)
+        time.sleep(yzdelay)
 
-def RectScan(length, width):
+def RectScan(length, width, ScanWidth):
     
-    PreStepConv = 0.0000125
-    TheStepConv = 0.000003175
     xStep = round(length/PreStepConv)
-    xIndent = round(xStep/20)
+    xIndent = round(xStep/ScanWidth)
     yStep = round(width/TheStepConv)
+    yIndent = round(yStep/ScanWidth)
 
     yDir = 0
+    pi.write(23, 1)     # x Dir ~~> away motor
+
     for indent in range(xIndent):
-        for x in range(20):
-            pi.write(23, 1)     # x Dir ~~> away motor
+        for x in range(ScanWidth):
             pi.write(24, 1)
-            time.sleep(0.0025)
+            time.sleep(xdelay)
             pi.write(24, 0)
-            time.sleep(0.0025)
+            time.sleep(xdelay)
 
         pi.write(27, yDir)     # y Dir ~~> initial away motor
-        for y in range(yStep):
-            pi.write(22, 1)
-            time.sleep(delay)
-            pi.write(22, 0)
-            time.sleep(delay)
+        for y in range(yIndent):
+            for y in range(ScanWidth):
+                pi.write(22, 1)
+                time.sleep(yzdelay)
+                pi.write(22, 0)
+                time.sleep(yzdelay)
+            time.sleep(1)       # This time would be taken to scan and record value
+                                # Insert Scan function here
         if yDir == 0:
             yDir = 1
         else:
--- PyFirmata Simple Stepper.py
+++ PyFirmata Simple Stepper.py
@@ -2,22 +2,23 @@ import pigpio
 import time
 from NDE_Stage_Utility import * # my custom stage library
 
-StartUpZero()
 
-
-# x, y, z are dist of home in m
-
-# start point
+# x, y, z are dist of home from zero in m
 x = 0.075
 y = 0.04
 z = 0.3
 
-MoveToHome(x, y, z)
-
-# length of specimim
-# rectangle
+# length and width of rectangle specimin in m
 length = 0.145
 width = 0.075
+# Steps between scans
+ScanWidth = 20
 
-RectScan(length, width)
 
+
+
+StartUpZero()
+
+MoveToHome(x, y, z)
+
+RectScan(length, width, ScanWidth)
